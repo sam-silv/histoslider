693d3a0d91468f6aab9ebb4e3c4d2dab59c3374f
Sam Hogg <sam@samhogg.com>
2015-10-11 09:28:25 +0900

    Updated example, added click behaviour to histogram

--- examples/main.js
+++ examples/main.js
@@ -35,19 +35,19 @@ class App extends Component {
 
   render () {
     const data = [1, 1, 1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 5, 5, 6, 7, 7, 7, 7, 8, 8, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 3, 2, 3, 3, 3, 3, 3]
-    let selection = this.state.selection ? arrays.extent([...this.state.selection]) : null
     return (
       <div>
         <div style={{ margin: '0 0 1rem 0' }}>
           <p>
             <b>Selection</b>
           </p>
-          <p>{ selection ? JSON.stringify(this.state.selection) : 'None' }</p>
-          <p>{ selection ? JSON.stringify(data.filter(m => { return m >= selection[0] && m <= selection[1]})) : 'None' }</p>
+          <p>{ this.state.selection ? JSON.stringify(this.state.selection) : 'None' }</p>
         </div>
         <Histoslider
           selection={this.state.selection}
           padding={20}
+          width={500}
+          height={200}
           data={data}
           onChange={this.histogramChanged.bind(this)}
         />
