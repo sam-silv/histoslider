5c116d7e3cee805dd7178b555bc45548ae921941
Sam Hogg <sam@samhogg.com>
2015-10-11 09:09:11 +0900

    Better output for demo

--- examples/main.js
+++ examples/main.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Histoslider from '../lib/components/Histoslider'
 
+import arrays from 'd3-arrays'
+
 const main = document.getElementById('main')
 
 class App extends Component {
@@ -32,18 +34,21 @@ class App extends Component {
   }
 
   render () {
+    const data = [1, 1, 1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 5, 5, 6, 7, 7, 7, 7, 8, 8, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 3, 2, 3, 3, 3, 3, 3]
+    let selection = this.state.selection ? arrays.extent([...this.state.selection]) : null
     return (
       <div>
         <div style={{ margin: '0 0 1rem 0' }}>
           <p>
             <b>Selection</b>
           </p>
-          <p>{ this.state && this.state.selection ? JSON.stringify(this.state.selection) : 'None' }</p>
+          <p>{ selection ? JSON.stringify(this.state.selection) : 'None' }</p>
+          <p>{ selection ? JSON.stringify(data.filter(m => { return m >= selection[0] && m <= selection[1]})) : 'None' }</p>
         </div>
         <Histoslider
           selection={this.state.selection}
           padding={20}
-          data={[1, 1, 1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 6, 7, 7, 7, 7, 8, 8, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 3, 2, 3, 3, 3, 3, 3]}
+          data={data}
           onChange={this.histogramChanged.bind(this)}
         />
       </div>
