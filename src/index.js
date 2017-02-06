3b4729e628f63c417456e4da9c99d094a8aaf1e5
Sam Hogg <sam.hogg@orionhealth.com>
2017-02-06 15:30:44 -0700

    Use storybook and create-react-app for development

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+import Histogram from './components/Histogram'
 import Histoslider from './components/Histoslider'
+import Slider from './components/Slider'
+
+export {
+  Histogram,
+  Histoslider,
+  Slider  
+}
+
 export default Histoslider
---
+++ src/stories/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import {
+  Histogram,
+  Histoslider,
+  Slider
+} from '..'
+
+storiesOf('Histogram', module)
+storiesOf('Slider', module)
+storiesOf('Histoslider', module)
